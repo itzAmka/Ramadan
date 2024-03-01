abb5358e146be62b2700e64116d73f3e0fbdfbf1
Amin <Aminhassen85@gmail.com>
2024-03-01 16:21:28 -0500

    refactor main.js

--- main.js
+++ main.js
@@ -1,33 +1,60 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-links li a');
-const logo = document.querySelector('.logo');
-const footerYear = document.querySelector('.footer-year');
+const logo = document.querySelector(".logo");
+const footerYear = document.querySelector(".footer-year");
 
 // colors
-const secondaryClr = '#ffdd83';
-const accentClr = '#98dfd6';
-
-hamburgerMenu.addEventListener('click', () => {
-	hamburgerMenu.classList.toggle('change');
-	navLinks.classList.toggle('active');
-
-	if (hamburgerMenu.classList.contains('change')) {
-		logo.style.color = secondaryClr;
-	} else {
-		logo.style.color = accentClr;
-	}
-
-	links.forEach(link => {
-		link.addEventListener('click', () => {
-			navLinks.classList.remove('active');
-			hamburgerMenu.classList.remove('change');
-			logo.style.color = accentClr;
-		});
-	});
-});
+const secondaryClr = "#52b788";
+const accentClr = "#e21818";
 
 footerYear.textContent =
-	new Date().getFullYear() == 2023
-		? '2023'
-		: `2023 - ${new Date().getFullYear()}`;
+  new Date().getFullYear() == 2023
+    ? "2023"
+    : `2023 - ${new Date().getFullYear()}`;
+
+function typingEffectWordsAnimation(className, newWords) {
+  const spanElement = document.querySelector(`.${className}`);
+
+  if (spanElement) {
+    const wordsArray = newWords;
+    let currentIndex = 0;
+
+    function animateWord(index) {
+      let word = wordsArray[index];
+      let partialWord = "";
+
+      for (let i = 0; i < word.length; i++) {
+        setTimeout(() => {
+          partialWord += word[i];
+          spanElement.textContent = partialWord;
+        }, i * 100);
+      }
+
+      setTimeout(
+        () => {
+          currentIndex = (currentIndex + 1) % wordsArray.length;
+          animateWord(currentIndex);
+        },
+        word.length * 100 + 3000,
+      );
+    }
+
+    // Start the animation
+    animateWord(currentIndex);
+  }
+}
+
+const words = [
+  "Mercy.",
+  "Forgiveness.",
+  "Reflection.",
+  "Gratitude.",
+  "Devotion.",
+  "Unity.",
+  "Blessings.",
+  "Empathy.",
+  "Sympathy.",
+  "Compassion.",
+  "Patience.",
+  "Renewal.",
+];
+
+typingEffectWordsAnimation("words", words);
